1570250b1ff6f4de3e1002421bf82b05317f143e
Anastasiia Romanenko <druzhkoshowewed@gmail.com>
2021-04-20 10:18:45 +0300

    Полиномиальное построение графика экспенимента + реальные данные + теперь без фитс

--- jpg_to_fits.py
+++ jpg_to_fits.py
@@ -16,43 +16,10 @@ def jpg_to_fits(filename):
     r_data = np.array(r.getdata()) # data is now an array of length ysize*xsize
     g_data = np.array(g.getdata())
     b_data = np.array(b.getdata())
-    # print(r_data.shape)
-
-    ##############################################################################
-    # Reshape the image arrays to be 2-dimensional:
-
-    r_data = r_data.reshape(ysize, xsize) # data is now a matrix (ysize, xsize)
-    g_data = g_data.reshape(ysize, xsize)
-    b_data = b_data.reshape(ysize, xsize)
-    # print(r_data.shape)
-
-    ##############################################################################
-    # Write out the channels as separate FITS images.
-    # Add and visualize header info
-
-    red = fits.PrimaryHDU(data=r_data)
-    red.header['LATOBS'] = "32:11:56" # add spurious header info
-    red.header['LONGOBS'] = "110:56"
-    red.writeto('red.fits')
-
-    green = fits.PrimaryHDU(data=g_data)
-    green.header['LATOBS'] = "32:11:56"
-    green.header['LONGOBS'] = "110:56"
-    green.writeto('green.fits')
-
-    blue = fits.PrimaryHDU(data=b_data)
-    blue.header['LATOBS'] = "32:11:56"
-    blue.header['LONGOBS'] = "110:56"
-    blue.writeto('blue.fits')
-
-    #red.writeto('images_fits/test_r.fits')
-    #green.writeto('images_fits/test_g.fits')
-    #blue.writeto('images_fits/test_b.fits')
-    ##############################################################################
-    # Delete the files created
-    r = np.sum(red.data)
-    b = np.sum(blue.data)
-    g = np.sum(green.data)
+    
+    r = np.sum(r_data)
+    b = np.sum(b_data)
+    g = np.sum(g_data)
     
     with open("raw_data.txt", "a") as myfile:
         myfile.write(' '.join([filename[:-4], str(r.item()), str(b.item()), str(g.item()), '\n']))
@@ -68,8 +35,7 @@ def clear_data():
 
     sorted_data = sorted(raw_data, key=lambda a: a[0], reverse=False)
     summed_data = [sum(data[1:]) for data in sorted_data]
-    middled_data = [i - min(summed_data) for i in summed_data]
-    clear_data = [i / max(middled_data) for i in middled_data]
+    clear_data = [i / max(summed_data) for i in summed_data]
 
     clear_data_file = open('data.txt', 'w')
     for data in clear_data:
--- show_data.py
+++ show_data.py
@@ -1,9 +1,34 @@
 import matplotlib.pyplot as plt
+import scipy as sp
 
-with open('data.txt', 'r') as f:
-    data = [float(i) for i in f.read().split()]
 
-plt.plot(data, label='brightness')
-plt.xlabel('time')
-plt.ylabel('% britness')
-plt.show()
+def mnkGP(x, y):
+    d = 4 # степень полинома
+    fp, residuals, rank, sv, rcond = sp.polyfit(x, y, d, full=True) # Модель
+    f = sp.poly1d(fp) # аппроксимирующая функция
+    y1=[fp[0] * x[i] ** 2 + fp[1] * x[i] + fp[2] for i in range(0, len(x))] # значения функции a*x**2+b*x+c
+    so = round(sum([abs(y[i] - y1[i]) for i in range(0, len(x))]) / (len(x) * sum(y)) * 100, 4) # средняя ошибка
+    print('Average quadratic deviation '+str(so)) 
+    fx = sp.linspace(x[0], x[-1] + 1, len(x)) # можно установить вместо len(x) большее число для интерполяции
+    plt.plot(x, y, label='Original data', markersize=10)
+    plt.plot(fx, f(fx), label='experiment model', linewidth=2)
+    plt.grid(True)
+    #plt.show()
+
+
+def show_data():
+    with open('data.txt', 'r') as f:
+        data = [float(i) for i in f.read().split()]
+
+    mnkGP(list(range(len(data))), data)
+
+
+def show_circle_model():
+    from models import circle_model
+    plt.plot(list(range(len(circle_model))), circle_model, 'r', label='model', markersize=10)
+
+
+if __name__ == '__main__':
+    show_circle_model()
+    show_data()
+    plt.show()
